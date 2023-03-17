8ece946de60736bd131c0b4d94f6a1012ec46e43
swuY <swu341457@gmail.com>
2023-03-17 02:47:08 +0100

    add comment system

--- source/js/modify.js
+++ source/js/modify.js
@@ -1,7 +1,8 @@
 
 
 
-function InsertTopImage()
+/* 更改post top-image 位置 */
+(function insertTopImage()
 {
     var TopImage = document.querySelector("#page-header").style.backgroundImage
     if (TopImage) {
@@ -14,6 +15,14 @@ function InsertTopImage()
         return ;
     }
 
-}
+})();
 
-InsertTopImage()
+/* 评论区加上 placeholder */
+(function addTextareaPlaceholder()
+{
+    window.addEventListener("load", function(event) {
+        let Textarea = document.querySelector(".el-textarea__inner");
+        Textarea.placeholder = "欢迎回家~ ~ \n留下点什么吧 ヾ(≧∇≦*)ゝ";
+    }); 
+})();
+
